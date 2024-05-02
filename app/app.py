b8bcaeadb8af5c88b708de46b9e1a591ccee44a3
DiegoBurgos <103340654+DABS03@users.noreply.github.com>
2024-05-01 19:26:41 -0600

    Segundo commit

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
@@ -12,7 +12,7 @@ DATABASE_PASSWORD = 'DABSceptile'
 
 SQLALCHEMY_DATABASE_URI = f'mssql+pyodbc://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}?driver=ODBC Driver 17 for SQL Server'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
+engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 Session = sessionmaker(bind=engine)
 
 # Función para obtener los pedidos en proceso
@@ -31,6 +31,7 @@ def obtener_pedidos_en_proceso():
         ORDER BY
             p.id_pedido;
     """)
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
     session = Session()
     resultados = session.execute(query).fetchall()
     session.close()
@@ -39,14 +40,15 @@ def obtener_pedidos_en_proceso():
 # Función para obtener los detalles de la tabla comida
 def obtener_detalles_comida():
     query = text("SELECT * FROM comida;")
-    session = Session()
+    session = Session();
     resultados = session.execute(query).fetchall()
+    session.commit()
     session.close()
     return resultados
 
 # Función para obtener los detalles de la tabla detalle_de_pedido
 def obtener_detalles_pedido():
-    query = text("SELECT * FROM detalle_de_pedido;")
+    query = text("""SELECT * FROM detalle_de_pedido;""")
     session = Session()
     resultados = session.execute(query).fetchall()
     session.close()
@@ -54,7 +56,7 @@ def obtener_detalles_pedido():
 
 # Función para obtener los detalles de la tabla pedido
 def obtener_estados_pedido():
-    query = text("SELECT * FROM pedido;")
+    query = text("""SELECT * FROM pedido;""")
     session = Session()
     resultados = session.execute(query).fetchall()
     session.close()
@@ -67,7 +69,15 @@ def index():
     detalles_pedido = obtener_detalles_pedido()
     estados_pedido = obtener_estados_pedido()
 
+    print("Número de registros en detalles_comida:", len(detalles_comida))
+
     return render_template('index.html', pedidos=pedidos, detalles_comida=detalles_comida, detalles_pedido=detalles_pedido, estados_pedido=estados_pedido)
 
+def pagina_no_encontrada(error):
+    # return render_template('404.html'), 404
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
+    app.register_error_handler(404, pagina_no_encontrada)
     app.run(debug=True, port=5000)
+    
